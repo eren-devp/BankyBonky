e61184a837d6e4a6b9cac7bf524cb9731991fe9b
eren-devp <talha.genc@bahcesehir.edu.tr>
2023-08-31 16:40:41 +0300

    Testing git.

--- BankyBonky/BankyBonky.cpp
+++ BankyBonky/BankyBonky.cpp
@@ -8,6 +8,7 @@ int main()
 	try {
 		Client client("Talha", "Genc", 18, 11111111111, "VIP Client");
 		Employee employee("Burak", "Ergul", 19, 11111111111, "Manager", 15000);
+		Client client2("eren", "genc", 19, 11111111111, "poor");
 
 		employee.ShowInformations();
 		cout << endl;
