9d6d820a52470255a19d54034d32a659b737baa8
eren-devp <talha.genc@bahcesehir.edu.tr>
2023-09-02 01:38:29 +0300

    Update BankyBonky.cpp

--- BankyBonky/BankyBonky.cpp
+++ BankyBonky/BankyBonky.cpp
@@ -8,10 +8,6 @@ int main()
 	try {
 		Client client = Client("Talha", "Genc", 18, 11111111111, "VIP Client");
 		Employee employee("Burak", "Ergul", 19, 11111111111, "Manager", 15000);
-
-		employee.ShowInformations();
-		cout << endl;
-		client.ShowInformations();
 	}
 	catch (exception out) {
 		cout << out.what() << endl; // If we had any exceptions out we need to see them.
